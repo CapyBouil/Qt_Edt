7bce94f3caa9adcf8ea0f481c45a89870e2bf77f
AugustinChamoussetESEO <augustin.chamousset@reseau.eseo.fr>
2025-01-16 10:26:18 +0100

    EDT

--- emploidutemps.cpp
+++ emploidutemps.cpp
@@ -1,54 +1,6 @@
-#include "ecue.h"
+#include "emploidutemps.h"
 
-#include <iostream>
-#include <string>
+EmploiDuTemps::EmploiDuTemps() {}
 
-using namespace std;
-
-ECUE::ECUE(std::string nomECUE, std::string typeECUE, float nbHeures) {
-    this->nomECUE = nomECUE;
-    this->nbHeures = nbHeures;
-    this->typeECUE = typeECUE;
-}
-
-//Acesseurs
-
-std::string ECUE::getNomECUE()
-{
-    return this->nomECUE;
-}
-void ECUE::setNomECUE(std::string nom)
-{
-    this->nomECUE = nom;
-}
-
-std::string ECUE::getTypeECUE()
-{
-    return this->typeECUE;
-}
-void ECUE::setTypeECUE(std::string type)
-{
-    this->typeUCUE = type;
-}
-
-float ECUE::getNbHeures()
-{
-    return this->nbHeures;
-}
-void ECUE::setNomECUE(float nb)
-{
-    this->nbHeures = nb;
-}
-
-
-
-//Methodes pour affecter un enseigant a un ECUE
-void ECUE::ajouterEnseignant(Enseignant e)
-{
-
-}
-
-void ECUE::spprimerEnseignant(Enseignant enseignant)
-{
-
-}
+void ajouteCreneau(){}
+void afficheEdt(){}
