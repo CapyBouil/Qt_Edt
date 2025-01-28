2b8e6d02acc7d219cf8b5dd8983ab9c69a54874b
Coco <corentinpineau25@gmail.com>
2025-01-28 11:41:47 +0100

    Style

--- style.cpp
+++ style.cpp
@@ -1,4 +1,3 @@
-
 #include "style.h"
 
     QString getGlobalStyle() {
@@ -23,10 +22,46 @@
         QListWidget {
             border: 1px solid #bdc3c7;
             background-color: #ecf0f1;
+            font-size: 14px;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        QListWidget::item {
+            padding: 15px; /* Ajouter un padding pour agrandir les éléments */
+            border-bottom: 1px solid #bdc3c7; /* Optionnel : ajouter une bordure entre les éléments */
+        }
+        QListWidget::item:hover {
+            background-color: #d5dbdb; /* Changer la couleur de fond au survol */
+        }
+        QListWidget::item:selected {
+            background-color: #3498db; /* Changer la couleur de fond pour l'élément sélectionné */
+            color: white;
         }
         QTableWidget {
             border: 1px solid #bdc3c7;
             gridline-color: #bdc3c7;
+        }
+        QTabBar::tab {
+            background-color: #3498db;  /* Fond bleu */
+            color: #ffffff;  /* Texte blanc */
+            padding: 10px;
+            border: 1px solid #e0e0e0;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+
+        QTabBar::tab:selected {
+            background-color: #1c5980;  /* Fond bleu clair pour l'onglet sélectionné */
+            color: #ffffff;  /* Texte blanc */
+        }
+
+        QTabBar::tab:hover {
+            background-color: #2980b9;  /* Fond bleu clair au survol */
+            color: #ffffff;  /* Texte blanc */
+        }
+
+        QSplitter::handle {
+            background-color: #3498db;  /* Fond bleu clair */
+            border: 1px solid #ffffff;
         }
     )";
 }
