332b6270eb8ced7bb3338832bddfb9796f83fdad
Anthony_Dh <adinhpsn@gmail.com>
2025-01-30 11:21:42 +0100

    find retour null (optional)

--- controleur.cpp
+++ controleur.cpp
@@ -2,7 +2,7 @@
 
 Controleur::Controleur() {}
 
-Enseignant Controleur::findEnseignantByNomPrenom(const QString& nomPrenom) const {
+std::optional<Enseignant> Controleur::findEnseignantByNomPrenom(const QString& nomPrenom) const {
     Factory::loadEnseignant();
     if (Factory::listeEnseignant.empty()) {
         qDebug() << "La liste des enseignants est vide.";
@@ -17,12 +17,11 @@ Enseignant Controleur::findEnseignantByNomPrenom(const QString& nomPrenom) const
             return enseignant;
         }
     }
-
     qDebug() << "Enseignant introuvable pour : " << nomPrenom;
-    throw std::runtime_error("Enseignant non trouve");
+    return std::nullopt;
 }
 
-Etudiant Controleur::findEtudiantByNomPrenom(const QString& nomPrenom) const {
+std::optional<Etudiant> Controleur::findEtudiantByNomPrenom(const QString& nomPrenom) const {
     Factory::loadEtudiant();
     if (Factory::listeEtudiant.empty()) {
         qDebug() << "La liste des Etudiants est vide.";
@@ -39,10 +38,10 @@ Etudiant Controleur::findEtudiantByNomPrenom(const QString& nomPrenom) const {
     }
 
     qDebug() << "Etudiant introuvable pour : " << nomPrenom;
-    throw std::runtime_error("Etudiant non trouve");
+    return std::nullopt;
 }
 
-Salle Controleur::findSalleByNumero(const QString& numeroComplet) const {
+std::optional<Salle> Controleur::findSalleByNumero(const QString& numeroComplet) const {
     Factory::loadSalle();
     if (Factory::listeSalle.empty()) {
         qDebug() << "La liste des Salles est vide.";
@@ -59,12 +58,12 @@ Salle Controleur::findSalleByNumero(const QString& numeroComplet) const {
     }
 
     qDebug() << "Salle introuvable pour : " << numeroComplet;
-    throw std::runtime_error("Salle non trouvee");
+    return std::nullopt;
 }
 
 
 
-ECUE Controleur::findECUEByNom(const QString& nomECUE) const {
+std::optional<ECUE> Controleur::findECUEByNom(const QString& nomECUE) const {
     Factory::loadEcue();
     if (Factory::listeEcue.empty()) {
         qDebug() << "La liste des ECUEs est vide.";
@@ -81,10 +80,10 @@ ECUE Controleur::findECUEByNom(const QString& nomECUE) const {
     }
 
     qDebug() << "ECUE introuvable pour : " << nomECUE;
-    throw std::runtime_error("ECUE non trouve");
+    return std::nullopt;
 }
 
-Classe Controleur::findClasseByNomClasse(const QString& nomClasse) const {
+std::optional<Classe> Controleur::findClasseByNomClasse(const QString& nomClasse) const {
 
     Factory::loadClasse();
 
@@ -109,8 +108,7 @@ Classe Controleur::findClasseByNomClasse(const QString& nomClasse) const {
     }
 
     qDebug() << "Classe introuvable pour : " << nomClasse;
-
-    throw std::runtime_error("Classe non trouvée");
+    return std::nullopt;
 
 }
 
--- controleur.h
+++ controleur.h
@@ -4,17 +4,19 @@
 
 #include "factory.h"
 #include <QDebug>
+#include <optional>
+
 
 class Controleur
 {
 public:
     Controleur();
     QString formaterMinuscule(QString texte);
-    Enseignant findEnseignantByNomPrenom(const QString& nomPrenom) const;
-    Etudiant findEtudiantByNomPrenom(const QString& nomPrenom) const;
-    Salle findSalleByNumero(const QString& numeroComplet) const;
-    ECUE findECUEByNom(const QString& nomECUE) const;
-    Classe findClasseByNomClasse(const QString& nomClasse) const;
+    std::optional<Enseignant> findEnseignantByNomPrenom(const QString& nomPrenom) const;
+    std::optional<Etudiant> findEtudiantByNomPrenom(const QString& nomPrenom) const;
+    std::optional<Salle> findSalleByNumero(const QString& numeroComplet) const;
+    std::optional<ECUE> findECUEByNom(const QString& nomECUE) const;
+    std::optional<Classe> findClasseByNomClasse(const QString& nomClasse) const;
 
 
 
