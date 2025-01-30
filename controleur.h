c79ae8b1884b7a2322da70463b85a282843d93b5
Coco <corentinpineau25@gmail.com>
2025-01-30 12:40:06 +0100

    controleur.h

--- controleur.h
+++ controleur.h
@@ -1,9 +1,10 @@
 #ifndef CONTROLEUR_H
 #define CONTROLEUR_H
-#include <QString>
 
 #include "factory.h"
 #include <QDebug>
+#include <optional>
+
 
 class Controleur
 {
@@ -12,7 +13,15 @@ public:
     QString formaterMinuscule(QString texte);
     Enseignant findEnseignantByNomPrenom(const QString& nomPrenom) const;
     Etudiant findEtudiantByNomPrenom(const QString& nomPrenom) const;
+    Salle findSalleByNumero(const QString& numeroComplet) const;
+    ECUE findECUEByNom(const QString& nomECUE) const;
+    Classe findClasseByNomClasse(const QString& nomClasse) const;
+    std::optional<Enseignant> findEnseignantByNomPrenom(const QString& nomPrenom) const;
+    std::optional<Etudiant> findEtudiantByNomPrenom(const QString& nomPrenom) const;
+    std::optional<Salle> findSalleByNumero(const QString& numeroComplet) const;
+    std::optional<ECUE> findECUEByNom(const QString& nomECUE) const;
+    std::optional<Classe> findClasseByNomClasse(const QString& nomClasse) const;
 
 };
 
-#endif // CONTROLEUR_H
+#endif
